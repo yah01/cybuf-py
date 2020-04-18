27306d82191e5a05a5d07c7aa084fe05b960fe0c
wh201906 <62299611+wh201906@users.noreply.github.com>
2020-04-19 01:03:54 +0800

    Replace bytes with str&Change variable name&Turn cybuf into a module

---
+++ cybuf/marshal.py
@@ -0,0 +1,43 @@
+# defaultEncoding = "utf-8"
+marshal_sep = " "
+
+def _marshal(obj, tab_count, with_indent):
+    cybuf_str = "{"
+    if with_indent:
+        cybuf_str += "\n"
+    tabs = ""
+    if with_indent:
+        tabs = "\t" * tab_count
+        
+    if type(obj) == dict:
+        for (k, v) in obj.items():
+            cybuf_str += tabs
+            cybuf_str += str(k)
+            cybuf_str += ":"
+            if with_indent:
+                cybuf_str += " "
+                
+            value_str = ""
+            print(type(v))
+            if type(v) == int:
+                value_str = str(v)
+            elif type(v) == float:
+                value_str = str(v)
+            elif type(v) == None:
+                value_str += "nil"
+            elif type(v) == bool:
+                value_str += str(v)
+            elif type(v) == str:
+                value_str += "\"" + v + "\""
+            cybuf_str += value_str
+            if with_indent:
+                cybuf_str += "\n"
+            else:
+                if type(v)!=str:
+                    cybuf_str += marshal_sep
+            cybuf_str += tabs[1:]
+        cybuf_str += "}"
+    return cybuf_str
+
+if __name__ == "__main__":
+    print("test mode, marshal.py")
